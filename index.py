f68271b7606635aad0857e44f207bd4f8f416e70
rahibvk <rahibmr1@gmail.com>
2024-07-11 19:07:03 +0300

    new function

--- index.py
+++ index.py
@@ -2,15 +2,24 @@ import random
 
 life = 5
 
-x = random.randrange(-1,11)
+range_start = int(input("Enter the starting range: "))
+range_end = int(input("Enter the ending range: "))
+
+x = random.randrange(range_start, range_end)
+
+near1 = x - 10
 
 
 
 while life <=5:
+    
     guess = int(input("Guess the number: "))
     if x == guess:
         print("You guessed it right")
         quit()
+    elif guess == near1:
+        print("You are near")
+        life -= 1
     elif life == 0:
         print("You lost")
         quit()
@@ -20,10 +29,7 @@ while life <=5:
     elif x < 5:
         print("the number is less than 5")
         life -= 1
-    elif life == 0:
-        print("You lost")
-        quit()
-        
+    
 
 
 
